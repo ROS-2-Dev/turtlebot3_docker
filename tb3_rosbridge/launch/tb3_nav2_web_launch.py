0c11c92f69f58b55eb17f6c5149e1fb3d375086e
Josef <josefgstoettner@live.at>
2024-03-08 09:35:44 +0800

    add rosbridge pkg

---
+++ tb3_rosbridge/launch/tb3_nav2_web_launch.py
@@ -0,0 +1,13 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_path
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(str(get_package_share_path('nav2_bringup')/ 'launch'/ 'tb3_simulation_launch.py'),
+                                launch_arguments={'headless': 'False'}.items()),
+
+        IncludeLaunchDescription(str(get_package_share_path('rosbridge_server')/ 'launch'/ 'rosbridge_websocket_launch.xml'),
+                                launch_arguments={'port': '9090'}.items()),
+    ])
